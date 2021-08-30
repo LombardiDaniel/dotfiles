9caf9aa978416f313eb43b3e7eff0ffe2c90142d
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2021-08-30 15:45:32 -0300

    added vimrc

---
+++ .vimrc
@@ -0,0 +1,18 @@
+syntax on
+
+set noerrorbells
+set tabstop=4 softtabstop=4
+set expandtab
+set smartindent
+set nu
+set nowrap
+set smartcase
+set noswapfile
+set nobackup
+set undodir=~/.vim/undodir
+set undofile
+
+call plug#begin('~/.vim/plugged')
+
+
+call plug#end()
--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ dotfiles=(
 gitconfig
 hyper.js
 zshrc
+vimrc
 envvars
 aliases
 )
@@ -14,6 +15,13 @@ for i in ${dotfiles[@]} ; do
     rm ~/.$i
     ln -snf ~/dotfiles/.$i ~/.$i
 done
+
+# Creates vim/undodir and installs plugin manager
+mkdir -p ~/.vim/undodir
+curl -fLo '~/.vim/autoload/plug.vim' --create-dirs \
+    'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
 
 # Confs
 confs=(
