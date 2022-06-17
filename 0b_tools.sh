1ef71b260bb0ada8a2fe80372764daac40fa2f3a
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2022-06-17 17:56:10 -0300

    Created 0b tools

---
+++ 0b_tools.sh
@@ -0,0 +1,26 @@
+# https://www.youtube.com/watch?v=3NTXFUxcKPc
+
+hexdump -C $FILE_NAME # get hexdump of file
+
+strings $FILE_NAME # get strings inside hex
+
+man syscalls
+
+strace $FILE_NAME # prints what gets executed (if you include parameter as well)
+
+ltrace $FILE_NAME # same but with library functions
+
+r2 $FILE_NAME # redare2: `?`: help | can also be used to debug program | Tab for auto-completion
+    aaa # automatically analyse and auto-name
+    afl # print all found functions
+    s sys.main # navigate to main function
+    pdf # print disassebly of current function
+    VV # enter visual mode -> Tab and Shift+Tab select boxes
+    p # change representation
+
+r2 -d $FILE_NAME # degub
+    aaa
+    afl
+    db $WHERE # plaecs breakpoint
+    VV
+    ;dc # runs program
